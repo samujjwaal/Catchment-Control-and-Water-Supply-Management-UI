4aa563cf6e94bf27fdb7e6edb11c5ccfb5a7ecb2
Samujjwaal Dey <sdey9@uic.edu>
2020-05-15 00:57:07 -0500

    Removed unnecessary files & organized repo

---
+++ num_mat.py
@@ -0,0 +1,11 @@
+list1 =[1,1,1,1,1]
+list2 =[2,2,2,2,2,2,2]
+list3=[3,3,3]
+list4=[4,4,4,4]
+list5=[]
+
+list5.append(list1)
+list5.append(list2)
+list5.append(list3)
+list5.append(list4)
+print(list5)
