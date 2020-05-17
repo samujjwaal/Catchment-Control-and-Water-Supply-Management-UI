a6d0d617d58517f2ecbf87380daf6f377960d3e8
Samujjwaal Dey <sdey9@uic.edu>
2020-05-16 20:43:47 -0500

    Replaced urllib.request with Requests for API call

--- API_TEST.py
+++ API_TEST.py
@@ -1,42 +1,42 @@
-import urllib.request
-import json
+# import dependencies
+import requests
+from requests.exceptions import HTTPError
 
-data = {
-        "Inputs": {
-                "Catchment_Input":
-                [
-                    {
-                            'LAT': input("LAT : "),   
-                            'LON': input("LON : "),   
-                            'RAINFALL': input("RAINFALL : "),   
-                            'GRDWTR': input("GRDWTR : "),   
-                            'Soil_Score': input("SOIL_SCORE : "),   
-                            'Elevation': input("ELEVATION : "),   
-                    }
-                ],
-        },
-    "GlobalParameters":  {
-    }
+# json data object to be sent to the web service API endpoint
+api_data = {
+    "Inputs": {
+        "Catchment_Input": [
+            {
+                "LAT": input("LAT : "),
+                "LON": input("LON : "),
+                "RAINFALL": input("RAINFALL : "),
+                "GRDWTR": input("GRDWTR : "),
+                "Soil_Score": input("SOIL_SCORE : "),
+                "Elevation": input("ELEVATION : "),
+            }
+        ],
+    },
+    "GlobalParameters": {},
 }
 
-body = str.encode(json.dumps(data))
-
-url = 'https://ussouthcentral.services.azureml.net/workspaces/e8133a8b3c3e4e70be72cffb0e45a85c/services/95b227a3833042df9afeb7abe8cbd71b/execute?api-version=2.0&format=swagger'
-api_key = 'LQ0Tb4vX0P7Fbb/zuGmNSauhlKrJv7WcKJps70psDo+8f1vKrV7GyX/XQCD5pJ4CUG/pNeQc/xZX+XK/T5RQkw==' # Replace this with the API key for the web service
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
-
-req = urllib.request.Request(url, body, headers)
+# Replace this with the URI and API Key for different web service
+url = "https://ussouthcentral.services.azureml.net/workspaces/e8133a8b3c3e4e70be72cffb0e45a85c/services/95b227a3833042df9afeb7abe8cbd71b/execute?api-version=2.0&format=swagger"
+api_key = "LQ0Tb4vX0P7Fbb/zuGmNSauhlKrJv7WcKJps70psDo+8f1vKrV7GyX/XQCD5pJ4CUG/pNeQc/xZX+XK/T5RQkw=="
+headers = {"Content-Type": "application/json", "Authorization": ("Bearer " + api_key)}
 
 try:
-    response = urllib.request.urlopen(req)
+    # POST request to API
+    response = requests.post(url=url, json=api_data, headers=headers)
 
-    result = response.read()
-##    op = response.json()
-##    print(op["Results"]["Catchment_Output"][0]["Scored Labels"])
+    # If the response was successful, no Exception will be raised
+    response.raise_for_status()
+
+    result = response.json()
+    score = result["Results"]["Catchment_Output"][0]["Scored Labels"]
     print(result)
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(json.loads(error.read().decode("utf8", 'ignore')))
+except HTTPError as http_error:
+    # Print the headers - they include the request ID and the timestamp, which are useful for debugging the failure
+    print(f"The request failed with status code: {http_error.response.status_code}\n")
+    print(f"Request Headers: {http_error.response.headers}\n")
+    print(f"Error Object: {http_error.response.json()}")
